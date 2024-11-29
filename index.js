503f31af952dcc5bdb5d3d35f69da0790673b853
elmirahmadov <elmir.ahmadov.2002@gmail.com>
2024-11-29 13:26:08 +0400

    new task

--- index.js
+++ index.js
@@ -1,98 +1,120 @@
 // Task 1
 
-const countVowel = (str) => {
-  const count = "AIOUEƏİÖÜ";
-  let vowelCount = 0;
-  for (let el of str.toUpperCase()) {
-    if (count.includes(el)) {
-      vowelCount++;
-    }
-  }
-  return vowelCount;
-};
-console.log("--------------------------");
-console.log("Azərbaycan", countVowel("Azərbaycan")); // Output: 3
+function printNumber(num) {
+  console.log(`The number is: ${num}`);
+}
+function performOperation(number, callback) {
+  callback(number);
+}
 
-const countVowel1 = (str1) => {
-  const count1 = "aıoueəiöü";
-  let vowelCount1 = 0;
-  for (let el of str1.toLowerCase()) {
-    if (count1.includes(el)) {
-      vowelCount1++;
-    }
-  }
-  return vowelCount1;
-};
+performOperation(10, printNumber); // Output: The number is: 10
 
 // Task 2
 
-console.log("Türkiyə", countVowel1("Türkiyə")); // Output: 3
-console.log("--------------------------");
-
-function printEvens(count) {
-  for (let i = 1; i <= count; i++) {
-    if (i % 2 === 0) {
-      console.log(i);
-    }
-  }
+function printTransformedArray(array, callback) {
+  const transformedArray = array.map(callback);
+  console.log(transformedArray);
 }
 
-printEvens(10); // Output: 2 4 6 8 10
-console.log("--------------------------");
-
-function printEvens1(count) {
-  for (let i = 2; i <= count; i += 2) {
-    console.log(i);
-  }
+function transformArray(num) {
+  return num * 2;
 }
-printEvens(10); // Output: 2 4 6 8 10
-console.log("--------------------------");
+
+const numbers = [1, 2, 3];
+
+printTransformedArray(numbers, transformArray); // Output: [2, 4, 6]
+
+// ----------------------------------------------------------------
+
+function printTransformedArray2(array, callback) {
+  const result = [];
+  for (let i = 0; i < numbers2.length; i++) {
+    result.push(callback(numbers2[i]));
+  }
+  console.log(result);
+}
+
+const numbers2 = [1, 2, 3];
+
+function transformArray2(num) {
+  return num * 2;
+}
+
+printTransformedArray2(numbers2, transformArray2); // Output: [2, 4, 6]
 
 // Task 3
 
-function sumDigits(num) {
-  const numString = num.toString();
-  let sum = 0;
-  for (let i = 0; i < numString.length; i++) {
-    sum += +numString[i];
-  }
-  return sum;
+function filterArray(array, callback) {
+  const filteredArray = array.filter(callback);
+  console.log(filteredArray);
+  return filteredArray;
 }
-console.log(sumDigits(123)); // Output: 15
-console.log("--------------------------");
-
-// Task 4
-function multiplicationTable(num) {
-  for (let i = 1; i <= 10; i++) {
-    console.log(`${num} x ${i} = ${num * i}`);
-  }
+function isEven(num) {
+  return num % 2 === 0;
 }
+const numbers3 = [1, 2, 3, 4];
+const filteredNumbers = filterArray(numbers3, isEven); // Output: [2, 4, 6]
 
-multiplicationTable(5);
-console.log("--------------------------");
+// ----------------------------------------------------------------
 
-function multiplicationTable1(num) {
-  for (let i = 1; i <= num; i++) {
-    console.log(`${num} x ${i} = ${num * i}`);
-  }
-}
-multiplicationTable1(10);
-
-// Task 5
-console.log("--------------------------");
-
-function fizzBuzz(num) {
-  for (let i = 1; i <= num; i++) {
-    if (i % 3 == 0 && i % 5 == 0) {
-      console.log("fizzBuzz");
-    } else if (i % 3 == 0) {
-      console.log("fizz");
-    } else if (i % 5 == 0) {
-      console.log("buzz");
-    } else {
-      console.log(i);
+function filterArray2(array, callback) {
+  const result = [];
+  for (let i = 0; i < array.length; i++) {
+    if (callback(array[i])) {
+      result.push(array[i]);
     }
   }
+  console.log(result);
+  return result;
 }
-fizzBuzz(15);
-console.log("--------------------------");
+function isEven2(num) {
+  return num % 2 === 0;
+}
+const numbers4 = [1, 2, 3, 4];
+const filteredNumbers2 = filterArray2(numbers4, isEven2); // Output: [2, 4, 6]
+
+// ----------------------------------------------------------------
+
+// Task 4
+
+// ----------------------------------------------------------------
+let num0 = [3, 8, 2, 10, 5];
+let maxNum = Math.max(...num0);
+console.log(maxNum);
+// ----------------------------------------------------------------
+let num1 = [3, 8, 2, 10, 5];
+let minNumber = Math.min(...num1);
+console.log(minNumber);
+// ----------------------------------------------------------------
+let num2 = 8.9;
+let floored = Math.floor(num2);
+console.log(floored);
+// ----------------------------------------------------------------
+let num3 = 4.1;
+let ceiled = Math.ceil(num3);
+console.log(ceiled);
+// ----------------------------------------------------------------
+let num4 = 6.75;
+let rounded = Math.round(num4);
+console.log(rounded);
+// ----------------------------------------------------------------
+let num5 = -20;
+let absoluteVal = Math.abs(num5);
+console.log(absoluteVal);
+// ----------------------------------------------------------------
+let num6 = 64;
+let sqrtValue = Math.sqrt(num6);
+console.log(sqrtValue);
+// ----------------------------------------------------------------
+let base = 5;
+let exponent = 3;
+let power = Math.pow(base, exponent);
+console.log(power);
+// ----------------------------------------------------------------
+let randomNumber = Math.random();
+console.log(randomNumber);
+// ----------------------------------------------------------------
+let num = 15.987;
+let truncated = Math.trunc(num);
+console.log(truncated);
+// ----------------------------------------------------------------
